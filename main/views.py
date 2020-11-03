35450faabce68b025361bdb157eef5f58622a2f4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-11-03 17:12:59 +0330

    fix

--- main/views.py
+++ main/views.py
@@ -5,10 +5,13 @@ import os
 from .models import GitApp
 import docker
 import urllib
+import io
 
 ROOT = '/home/keyvan/sites'
 doc = docker.from_env()
 
+
+HOP_BY_HOP = ['Connection','Keep-Alive','Proxy-Authenticate','Proxy-Authorization','TE','Trailers','Transfer-Encoding','Upgrade']
 def proxy(request, path, port):
     url = "http://127.0.0.1:{}{}".format(port, path)
     if request.GET:
@@ -17,28 +20,34 @@ def proxy(request, path, port):
         s = s.replace('HTTP_','',1)
         s = s.replace('_','-')
         return s
+
     request_headers = dict((convert(k),v) for k,v in request.META.items() if k.startswith('HTTP_'))
     request_headers['CONTENT-TYPE'] = request.META.get('CONTENT_TYPE', '')
     request_headers['CONTENT-LENGTH'] = request.META.get('CONTENT_LENGTH', '')
+
     if request.method == "GET":
         data = None
     else:
         data = request.raw_post_data
     downstream_request = urllib.request.Request(url, data, headers=request_headers)
-    page = urllib.request.urlopen(downstream_request)
-    response = HttpResponse(page)
-    return response
+    response = urllib.request.urlopen(downstream_request)
+    r = HttpResponse(response.read())
+    for header in response.info().keys():
+        if header not in HOP_BY_HOP:
+            r[header] = response.info()[header]
+    return r
 
-def serve_80(path, uid):
+def run(path, uid, port):
+    link = '{}/tcp'.format(port)
     try:
         cont = doc.containers.get(uid)
         if cont.status != 'running':
             cont.start()
     except docker.errors.NotFound:
         doc.images.build(path=path, tag=uid)
-        cont = doc.containers.run(uid, ports={'80/tcp': None}, detach=True, name=uid)
+        cont = doc.containers.run(uid, ports={link: None}, detach=True, name=uid)
 
-    return int(cont.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort'])
+    return int(cont.attrs['NetworkSettings']['Ports'][link][0]['HostPort'])
 
 def serve(request, user, repo, rest):
 
@@ -57,6 +66,12 @@ def serve(request, user, repo, rest):
     o = r.remotes.origin
     o.pull()
 
-    port = serve_80(repo_dir, uid)
+    if os.path.exists(os.path.join(repo_dir, '.gitabr')):
+        with io.open(os.path.join(repo_dir, '.gitabr')) as f:
+            web_port = int(f.read())
+    else:
+        web_port = 80
+
+    port = run(repo_dir, uid, web_port)
 
     return proxy(request, rest, port)
